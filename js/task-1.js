bd6a37176a55972f3c63a691eca7a44196796f4a
Nataliia Husak <nataliy.husak@gmail.com>
2019-11-03 11:57:36 +0200

    1
    04

--- js/task-1.js
+++ js/task-1.js
@@ -6,7 +6,7 @@ console.log(`В списке ${countOfCategories.length} категории`);
 
 const items = document.querySelectorAll(".item");
 
-items.forEach.call(items, element => {
+items.forEach(element => {
   const title = element.querySelector("h2").innerHTML;
   const countOfElements = element.querySelectorAll("li").length;
   console.log(`Категория: ${title};
